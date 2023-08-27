816bfc6bee21486275ee202d542295e7992a069f
Влад Шишов <shishovlad1@gmail.com>
2023-08-27 15:05:50 +0700

    Сборка новой архитектуры

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
 
 const app = express()
 const PORT = 3000
 
 app.use(express.static('./dist'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist/index.html'))
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
